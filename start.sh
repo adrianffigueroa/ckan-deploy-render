c34a43148a557c4e02892c32082781e085525777
adrianffigueroa <adrianffigeroa@gmail.com>
2025-06-04 16:59:52 -0300

    Usar gunicorn para exponer CKAN en Render

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 export CKAN_INI=ckan.ini
 
-# Usar el puerto que Render asigna
+# Usar Gunicorn con PasteDeploy
 PORT=${PORT:-5000}
-
-cd src/ckan
-python3 setup.py develop
-python3 -m paste.script.serve ../../ckan.ini --port=$PORT
+gunicorn --paste $CKAN_INI --bind 0.0.0.0:$PORT
