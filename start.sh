276ccb4ab50bac3dc2ce3fa1c18596365f0813c7
adrianffigueroa <adrianffigeroa@gmail.com>
2025-06-04 16:43:48 -0300

    CKAN escucha en el puerto dinámico que Render define

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 export CKAN_INI=ckan.ini
+
+# Usar el puerto que Render asigna
+PORT=${PORT:-5000}
+
 cd src/ckan
 python3 setup.py develop
-python3 -m paste.script.serve ../../ckan.ini
+python3 -m paste.script.serve ../../ckan.ini --port=$PORT
